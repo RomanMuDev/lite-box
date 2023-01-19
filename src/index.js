622efec74ad33562171902d86ea065be6cfc7074
RomaIT1 <life13062018@gmail.com>
2023-01-19 20:37:48 +0200

    added the vue controller

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import App from "./root/App";
+import Controller from "./root/controller";
 
 const app = Vue.createApp(App);
 
 app.mount("#root");
+Controller.init();
---
+++ src/root/controller.js
@@ -0,0 +1,9 @@
+class Controller {
+  init() {
+    console.log("init vue controller");
+  }
+}
+
+const controller = new Controller();
+
+export default controller;
